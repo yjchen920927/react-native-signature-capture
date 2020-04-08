1fb79d4bc2bd4b18556bfdbbc8bfe1b0ae56909c
yjchen920927 <yjchen_study@163.com>
2020-04-08 14:34:07 +0800

    Update SignatureCapture.js

--- SignatureCapture.js
+++ SignatureCapture.js
@@ -116,7 +116,8 @@ SignatureCapture.propTypes = {
     minStrokeWidth: PropTypes.number,
     maxStrokeWidth: PropTypes.number,
     strokeColor: PropTypes.string,
-    backgroundColor: PropTypes.string
+    backgroundColor: PropTypes.string,
+    itemName: PropTypes.string
 };
 
 var RSSignatureView = requireNativeComponent('RSSignatureView', SignatureCapture, {
